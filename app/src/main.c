5cfb164d33e47f357079afaa0bb70630b988b901
Adrian Suliga <adriansuliga81@gmail.com>
2026-09-25 18:27:45 +0200

    Solve l5-task1

--- app/src/main.c
+++ app/src/main.c
@@ -1,12 +1,16 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/zbus/zbus.h>
+#include <zephyr/task_wdt/task_wdt.h>
 
 LOG_MODULE_REGISTER(l4_task1, LOG_LEVEL_DBG);
 
 #define PRIORITY 5
 #define STACK_SIZE 1024
-#define SUBSCRIBER_QUEUE_SIZE 5
+#define QUEUE_SIZE 20
+#define HEALTH_PERIOD  100
+#define PRODUCER_SPEED 100
+#define CONSUMER_SPEED 5000
+#define WDT_TIMEOUT (2 * QUEUE_SIZE * PRODUCER_SPEED)
 
 struct sensor_data {
     uint32_t id;
@@ -14,58 +18,43 @@ struct sensor_data {
     uint32_t val2;
 };
 
-static void sensor_listener_cb(const struct zbus_channel *chan);
+K_MSGQ_DEFINE(sensor_queue, sizeof(struct sensor_data), QUEUE_SIZE, 4);
 
-ZBUS_LISTENER_DEFINE(sensor_listener, sensor_listener_cb);
-ZBUS_SUBSCRIBER_DEFINE(sensor_subscriber, SUBSCRIBER_QUEUE_SIZE);
-
-ZBUS_CHAN_DEFINE(sensor_chan, struct sensor_data, NULL, NULL,
-                 ZBUS_OBSERVERS(sensor_listener, sensor_subscriber),
-                 ZBUS_MSG_INIT(.id = 0, .val1 = 0, .val2 = 0));
-
-static void sensor_listener_cb(const struct zbus_channel *chan)
+static void task_wdt_callback(int channel_id, void *user_data)
 {
-    const struct sensor_data *data = 
-        (const struct sensor_data*)zbus_chan_const_msg(chan);
+    ARG_UNUSED(user_data);
 
-    LOG_INF("[LISTENER] thread %s processed msg [%d](%d,%d)",
-            k_thread_name_get(k_current_get()),
-            data->id,
-            data->val1,
-            data->val2);
+    LOG_INF("[WDT] Watchdog callback fires for channel %d. Consumer thread took too long.", channel_id);
 }
 
-static void sensor_subscriber_thread(void *p1, void *p2, void *p3)
+static void sensor_consumer_thread(void *p1, void *p2, void *p3)
 {
     ARG_UNUSED(p1);
     ARG_UNUSED(p2);
     ARG_UNUSED(p3);
 
-    const struct zbus_channel *chan;
+    task_wdt_init(NULL);
+    int channel_id = task_wdt_add(WDT_TIMEOUT, task_wdt_callback, NULL);
+
+    struct sensor_data data;
 
     while (true) {
-        int ret = zbus_sub_wait(&sensor_subscriber, &chan, K_FOREVER);
+        int ret = k_msgq_get(&sensor_queue, &data, K_FOREVER);
         if (ret != 0) {
-            LOG_INF("[SUBSCRIBER] Error when waiting for channel, %d", ret);
+            LOG_INF("[SUBSCRIBER] Error when waiting for message, %d", ret);
             break;
         }
 
-        struct sensor_data data;
-
-        ret = zbus_chan_read(chan, &data, K_FOREVER);
-        if (ret != 0) {
-            LOG_WRN("[SUBSCRIBER] Read failed with error %d", ret);
-            continue;
-        }
-
-        LOG_INF("[SUBSCRIBER] thread %s processed msg [%d](%d,%d)",
+        LOG_INF("[SUBSCRIBER] thread %s processing msg [%d](%d,%d)",
                 k_thread_name_get(k_current_get()),
                 data.id,
                 data.val1,
                 data.val2);
         
-        // Simulate busy work.
-        k_sleep(K_MSEC(80));
+        // Simulate busy work that will fill the queue up
+        k_sleep(K_MSEC(CONSUMER_SPEED));
+
+        task_wdt_feed(channel_id);
     }
 }
 
@@ -84,13 +73,31 @@ static void sensor_producer_thread(void *p1, void *p2, void *p3)
 
         LOG_INF("[SENSOR] Publish [%d](%d,%d)", data.id, data.val1, data.val2);
 
-        zbus_chan_pub(&sensor_chan, &data, K_NO_WAIT);
+        k_msgq_put(&sensor_queue, &data, K_NO_WAIT);
 
-        k_sleep(K_MSEC(100));
+        k_sleep(K_MSEC(PRODUCER_SPEED));
     }
 }
 
-K_THREAD_DEFINE(subscriber_thread, STACK_SIZE, sensor_subscriber_thread,
+static void sensor_health_check_thread(void *p1, void *p2, void *p3)
+{
+    ARG_UNUSED(p1);
+    ARG_UNUSED(p2);
+    ARG_UNUSED(p3);
+
+    while (true) {
+        int queue_fill = k_msgq_num_used_get(&sensor_queue);
+        if (4 * queue_fill > 3 * QUEUE_SIZE) {
+            LOG_WRN("Queue is becoming full - %d/%d", queue_fill, QUEUE_SIZE);
+        }
+
+        k_sleep(K_MSEC(HEALTH_PERIOD));
+    }
+}
+
+K_THREAD_DEFINE(consumer_thread, STACK_SIZE, sensor_consumer_thread,
                 NULL, NULL, NULL, PRIORITY, 0, 0);
 K_THREAD_DEFINE(producer_thread, STACK_SIZE, sensor_producer_thread,
                 NULL, NULL, NULL, PRIORITY, 0, 0);
+K_THREAD_DEFINE(health_thread, STACK_SIZE, sensor_health_check_thread,
+                NULL, NULL, NULL, PRIORITY + 1, 0, 0);
